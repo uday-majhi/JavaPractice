9b2da6d0cf653418515feeb08393d49371a60d87
uday-majhi <umajhi1@gmail.com>
2024-06-02 15:08:46 +0545

    Added new files

--- src/main/java/newfile.java
+++ src/main/java/newfile.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class newfile {
     public static void main(String[] args) {
         System.out.println("Hello");
+        int a = 2;
+        int b = 3;
+        System.out.println(a+b);
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Username");
+
+        String userName = myObj.nextLine();
+        System.out.println("Username is :" +userName);
+
+
     }
 }
